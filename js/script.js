212140b99ee3038dd92623e1c690c4821f0224db
Ola Ciesielska <aleksandra.ciesielska@op.pl>
2023-05-05 10:10:36 +0200

    #5 - Slajder glowny

---
+++ js/script.js
@@ -0,0 +1,21 @@
+let slide_index = 1;
+        
+        function nextSlide(n) {
+            displaySlides(slide_index += n);
+        }
+
+        function currentSlide(n) {
+            displaySlides(slide_index = n);
+        }
+
+        function displaySlides(n) {
+            let i;
+            let slides = document.getElementsByClassName("showSlide");
+            if (n > slides.length) { slide_index = 1 }
+            if (n < 1) { slide_index = slides.length }
+            for (i = 0; i < slides.length; i++) {
+                slides[i].style.display = "none";
+            }
+            slides[slide_index - 1].style.display = "block";
+        }
+    displaySlides(slide_index);
