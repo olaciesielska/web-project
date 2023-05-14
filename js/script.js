9c4c80d7a5a8cce66e0feb9ba47796096c4ccd4c
Ola Ciesielska <aleksandra.ciesielska@op.pl>
2023-05-14 20:28:06 +0200

    #2 #5 #6 update

--- js/script.js
+++ js/script.js
@@ -1,25 +1,37 @@
 let slide_index = 1;
+let left_arrow = document.querySelector(".left");
+let right_arrow = document.querySelector(".right");
 
 function nextSlide(n) {
   displaySlides((slide_index += n));
 }
 
-function currentSlide(n) {
-  displaySlides((slide_index = n));
-}
-
 function displaySlides(n) {
   let i;
-  let slides = document.getElementsByClassName("slider__element");
+  let slides = document.querySelectorAll(".slider__element");
+
   if (n > slides.length) {
     slide_index = 1;
   }
+
   if (n < 1) {
     slide_index = slides.length;
   }
+
   for (i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";
   }
+
   slides[slide_index - 1].style.display = "block";
 }
+
 displaySlides(slide_index);
+
+
+left_arrow.addEventListener("click", () => {
+  nextSlide(-1);
+});
+
+right_arrow.addEventListener("click", () => {
+  nextSlide(1);
+});
