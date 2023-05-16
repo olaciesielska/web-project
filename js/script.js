f5916f1888ad13e6895393e956928f263bc2bf21
Ola Ciesielska <aleksandra.ciesielska@op.pl>
2023-05-16 15:00:42 +0200

    #7 - Rwd

--- js/script.js
+++ js/script.js
@@ -2,6 +2,10 @@ let slide_index = 1;
 let left_arrow = document.querySelector(".left");
 let right_arrow = document.querySelector(".right");
 
+let minus_buttons = document.querySelectorAll(".productsSlider__input--minus");
+let plus_buttons = document.querySelectorAll(".productsSlider__input--plus");
+let input_fields = document.querySelectorAll(".productsSlider__input");
+
 function nextSlide(n) {
   displaySlides((slide_index += n));
 }
@@ -24,9 +28,7 @@ function displaySlides(n) {
 
   slides[slide_index - 1].style.display = "block";
 }
-
 displaySlides(slide_index);
-
 
 left_arrow.addEventListener("click", () => {
   nextSlide(-1);
@@ -34,4 +36,21 @@ left_arrow.addEventListener("click", () => {
 
 right_arrow.addEventListener("click", () => {
   nextSlide(1);
+});
+
+input_fields.forEach((input_field, index) => {
+  let currentValue = parseInt(input_field.value);
+
+  function changeValue(n) {
+    currentValue += n;
+    input_field.value = currentValue;
+  }
+
+  minus_buttons[index].addEventListener("click", () => {
+    changeValue(-1);
+  });
+
+  plus_buttons[index].addEventListener("click", () => {
+    changeValue(1);
+  });
 });
