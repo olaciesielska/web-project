a724ad1859d68882e192c6c6320458bede20937a
Ola Ciesielska <aleksandra.ciesielska@op.pl>
2023-05-18 15:05:00 +0200

    #5 Slajder glowny - mobile

--- js/script.js
+++ js/script.js
@@ -1,62 +1,67 @@
-let slide_index = 1;
-
-const left_arrow = document.querySelector(".left");
-const right_arrow = document.querySelector(".right");
-
-const minus_buttons = document.querySelectorAll(".productsSlider__input--minus");
-const plus_buttons = document.querySelectorAll(".productsSlider__input--plus");
-const input_fields = document.querySelectorAll(".productsSlider__input");
-
-function nextSlide(n) {
-  displaySlides((slide_index += n));
-}
-
-function displaySlides(n) {
-  let i;
+(() => {
   const slides = document.querySelectorAll(".slider__element");
+  let slide_index = 1;
 
-  if (n > slides.length) {
-    slide_index = 1;
+  const left_arrow = document.querySelector(".left");
+  const right_arrow = document.querySelector(".right");
+
+  function nextSlide(n) {
+    displaySlides((slide_index += n));
   }
 
-  if (n < 1) {
-    slide_index = slides.length;
-  }
+  function displaySlides(n) {
+    let i;
 
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
-  }
-
-  slides[slide_index - 1].style.display = "block";
-}
-displaySlides(slide_index);
-
-left_arrow.addEventListener("click", () => {
-  nextSlide(-1);
-});
-
-right_arrow.addEventListener("click", () => {
-  nextSlide(1);
-});
-
-input_fields.forEach((input_field, index) => {
-  let currentValue = parseInt(input_field.value);
-
-  function changeValue(n) {
-    currentValue += n;
-
-    if (currentValue < 1) {
-      currentValue = 1;
+    if (n > slides.length) {
+      slide_index = 1;
     }
 
-    input_field.value = currentValue;
+    if (n < 1) {
+      slide_index = slides.length;
+    }
+
+    for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+    }
+
+    slides[slide_index - 1].style.display = "block";
   }
 
-  minus_buttons[index].addEventListener("click", () => {
-    changeValue(-1);
+  displaySlides(slide_index);
+
+  left_arrow.addEventListener("click", () => {
+    nextSlide(-1);
   });
 
-  plus_buttons[index].addEventListener("click", () => {
-    changeValue(1);
+  right_arrow.addEventListener("click", () => {
+    nextSlide(1);
   });
-});
+})();
+
+(() => {
+  const minus_buttons = document.querySelectorAll(".productsSlider__input--minus");
+  const plus_buttons = document.querySelectorAll(".productsSlider__input--plus");
+  const input_fields = document.querySelectorAll(".productsSlider__input");
+
+  input_fields.forEach((input_field, index) => {
+    let currentValue = parseInt(input_field.value);
+
+    function changeValue(n) {
+      currentValue += n;
+
+      if (currentValue < 1) {
+        currentValue = 1;
+      }
+
+      input_field.value = currentValue;
+    }
+
+    minus_buttons[index].addEventListener("click", () => {
+      changeValue(-1);
+    });
+
+    plus_buttons[index].addEventListener("click", () => {
+      changeValue(1);
+    });
+  });
+})();
