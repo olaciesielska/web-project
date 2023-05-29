b3f56a9449380f69d1c6d68d4a4bca8b3391ff7a
Ola Ciesielska <aleksandra.ciesielska@op.pl>
2023-05-29 15:11:20 +0200

    #17 desktop, rwd do 992px

--- js/productsSlider.js
+++ js/productsSlider.js
@@ -48,31 +48,3 @@
     }
   });
 })();
-
-(() => {
-  const minus_buttons = document.querySelectorAll(".productsSlider__input--minus");
-  const plus_buttons = document.querySelectorAll(".productsSlider__input--plus");
-  const input_fields = document.querySelectorAll(".productsSlider__input");
-
-  input_fields.forEach((input_field, index) => {
-    let currentValue = parseInt(input_field.value);
-
-    function changeValue(n) {
-      currentValue += n;
-
-      if (currentValue < 1) {
-        currentValue = 1;
-      }
-
-      input_field.value = currentValue;
-    }
-
-    minus_buttons[index].addEventListener("click", () => {
-      changeValue(-1);
-    });
-
-    plus_buttons[index].addEventListener("click", () => {
-      changeValue(1);
-    });
-  });
-})();
